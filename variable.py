e7a6068f7f7eee3190b5da4f6b02f41fcd714a31
Teroces1 <jdaniel2007@icloud.com>
2026-01-30 02:11:34 -0500

    Created variable.py

---
+++ variable.py
@@ -0,0 +1,21 @@
+class Variable:
+    __slots__ = ("name")
+    def __setattr__(self, name, value):
+        if hasattr(self, name):
+            raise AttributeError("Fraction is immutable")
+        super().__setattr__(name, value)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+    
+    def __repr__(self):
+        return f"Variable(\"{self.name}\")"
+    
+    def __hash__(self):
+        return hash(self.name)
+    
+    def __invert__(self): # quick access for name
+        return self.name
