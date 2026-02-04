072b36f017a01514e5166080965d1ae7df0c799a
Golubkov Danila <LQ.H4@pcsb.org>
2026-02-04 10:05:26 -0500

    fix

--- ParserButGood/parser.py
+++ ParserButGood/parser.py
@@ -1,6 +1,6 @@
 #Shunting Yard parser
 from ParserButGood.token import Token 
-
+# infix to postfix -> 3 + 4 * 2 => 3 4 2 + *
 class Parser:
     def __init__(self, tokens):
         self.tokens=tokens
@@ -20,5 +20,16 @@ class Parser:
                 continue
             if token.type == "NUMBER":
                 self.output_queue.append(token)
-
-    
+            elif token.type =="FUNCTION":
+                self.operator_stack.append(token)
+                #todo parentheses logic
+            elif token.type == "COMMA": 
+                # Pop until we find a left parenthesis 
+                while self.operator_stack and self.operator_stack[-1].type != "LPAREN": 
+                    self.output_queue.append(self.operator_stack.pop()) 
+                    # If we never found a left parenthesis, it's an error 
+                    if not self.operator_stack: 
+                        raise ValueError("Misplaced comma or missing left parenthesis")
+            elif token.type=="OPERATOR":
+                return
+                #to do
