a442613f2ff5116a7363a4bfa2df0991987ffea4
Danila Laptop <danilgolubkov172@gmail.com>
2026-02-06 20:41:34 -0500

    working on parser

--- ParserButGood/parser.py
+++ ParserButGood/parser.py
@@ -2,18 +2,25 @@
 from ParserButGood.token import Token 
 # infix to postfix -> 3 + 4 * 2 => 3 4 2 + *
 class Parser:
-    def __init__(self, tokens):
-        self.tokens=tokens
-        self.operator_stack=[]
-        self.output_queue=[]
-        PRECEDENCE_LEVEL={
+    PRECEDENCE = {
             '+':2,
             '-':2,
             '*':3,
             '/':3,
             '^':4
         }
-
+    def __init__(self, tokens):
+        self.tokens=tokens
+        self.operator_stack=[]
+        self.output_queue=[]
+    def precedence(self, token):
+        if isinstance(token, Token):
+            return self.PRECEDENCE.get(token.value, -1)
+    def left_association(self, token):
+        if isinstance(token, Token):
+            if token.value == "^":
+                return True
+            return False
     def shunting_yard(self):
         for token in self.tokens:
             if not isinstance(token, Token):
@@ -31,5 +38,14 @@ class Parser:
                     if not self.operator_stack: 
                         raise ValueError("Misplaced comma or missing left parenthesis")
             elif token.type=="OPERATOR":
-                return
-                #to do
+                self.operator_stack.append(token)
+                #Find precedence level of prev and current token
+                if self.precedence(token) < self.precedence(self.operator_stack[len(self.operator_stack)-1]):
+                    self.operator_stack.pop()
+                    #change
+
+                elif self.precedence(token) == self.precedence(self.operator_stack[len(self.operator_stack)-1]) and self.left_association(token): #check equality of precedence and left association
+                    self.operator_stack.pop()
+                else:
+                    #end loop
+                    return
